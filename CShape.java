8351a32762e29fd38fa3d87401a8f39df8366770
nihaodo <145427011+nihaodo@users.noreply.github.com>
2023-10-31 16:20:22 +0800

    Add files via upload

---
+++ CShape.java
@@ -0,0 +1,27 @@
+abstract class CShape{
+    protected String color;
+    public void setColor(String str){
+        color = str;
+    }
+
+
+    public abstract void show();
+}
+
+
+CTriangle.java
+class CTriangle extends CShape{
+    double ca, cb, cc;
+    public CTriangle(double a, double b, double c){
+        ca=a;
+        cb=b;
+        cc=c;
+    }
+
+    public void show() {
+
+        System.out.print("color="+color+"  ");
+        System.out.print("area="+0.5cacb);
+    }
+
+}
